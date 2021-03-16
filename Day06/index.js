c1b4dda6ce9e00c09e471d24c09ab0b70e06df51
Park Na Yeon <within197@naver.com>
2021-03-16 17:58:53 +0900

    day06

--- Day06/index.js
+++ Day06/index.js
@@ -1,15 +1,23 @@
 const select = document.querySelector("select");
-const options = select.querySelectorAll("option");
 
-const KEY = "contry";
+function handleChange() {
+  const selected = select.value;
+  localStorage.setItem("country", selected);
+}
 
-function test() {
-  const value = select.value;
-  localStorage.setItem(KEY, value);
+function loadCountries() {
+  const selected = localStorage.getItem("country");
+  if (selected) {
+    const option = select.querySelector(`option[value="${selected}"]`);
+    //value값과 맞는 option찾기
+    option.selected = true;
+    //option의 selected 속성에 true를 넣어서 초기에 이 옵션을 선택한 상태로 설정함
+  }
 }
 
 function init() {
-  select.addEventListener("change", test);
+  loadCountries();
+  select.addEventListener("change", handleChange);
 }
 
 init();
